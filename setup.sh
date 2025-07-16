61e25adae9b8f4781fd72110d4876e6983781359
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 13:47:26 -0400

    Update startup.sh
    make basic structure and make ubuntu

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
 
-echo $(sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/')
+
 OS=$(sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/')
-echo $OS
+
+if [OS == "Debian GNU/Linux" ]; then
+    sudo apt update && sudo apt intall -y
+
+elif [[ $OS == "Ubuntu" ]]; then
+    sudo apt update && sudo apt install -y
+    sudo apt install openjdk-8-jre-headless
+    sudo apt install openjdk-17-jre-headless
+    sudo apt install openjdk-21-jre-headless
+
+elif [[ $OS == "Fedora" ]]; then
+
+elif [[ $OS == "Arch Linux" ]]; then
+
+elif [[ $OS == "Manjaro Linux" ]]; then
+
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
