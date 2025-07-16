caf83515f361f6df5ce109bb6684a61919366394
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 16:42:06 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 import os
 
-os.chdir("Instances")
-print(os.listdir())
-
+folder_path = os.path.join(os.path.dirname(__file__), 'Instances')
+if os.path.exists(folder_path):
+    directories = [entry.name for entry in os.scandir(folder_path) if entry.is_dir()]
+    print(directories)
+else:
+    print(f"Directory '{folder_path}' does not exist.")
 
 
 #java -Xmx1024M -Xms1024M -jar server.jar nogui
