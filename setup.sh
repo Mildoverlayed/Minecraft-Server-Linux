d1b17f9ce71d578f7540daadcfd01f53d589aaf8
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 13:58:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 OS=$(sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/')
 echo "Detected OS: $OS"
 
-if [$OS == "Debian GNU/Linux" ]; then
+if [ $OS == "Debian GNU/Linux" ]; then
     sudo apt update && sudo apt install -y
 
 elif [[ $OS == "Ubuntu" ]]; then
