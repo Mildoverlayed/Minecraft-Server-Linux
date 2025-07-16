9ada2989cfdebf91676212405940ec3a7352de3a
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 16:54:41 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,53 @@
+#import necessary modules
 import os
 
-folder_path = os.path.join(os.path.dirname(__file__), 'Instances')
-if os.path.exists(folder_path):
-    directories = [entry.name for entry in os.scandir(folder_path) if entry.is_dir()]
-    print(directories)
-else:
-    print(f"Directory '{folder_path}' does not exist.")
+
+
+
+# Functions
+def ListInstances():
+    """
+    List all Minecraft server instances.
+    """
+    folder_path = os.path.join(os.path.dirname(__file__), 'Instances')
+    if os.path.exists(folder_path):
+        directories = [entry.name for entry in os.scandir(folder_path) if entry.is_dir()]
+        print(directories)
+    else:
+        print("No instances found. Please create an instance folder in the Instances directory.")
+
+
+
+# Start of the script
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("Minecraft Server Manager")
+print("choose an option:")
+print("1. Create Instance")
+print("2. Start Instance")
+print("3. Delete Instance")
+print("4. Configure Instance")
+print("5. Global Settings")
+input_choice = int(input())
+
+if input_choice == 1:
+    pass
+elif input_choice == 2:
+    pass
+elif input_choice == 3:
+    pass
+elif input_choice == 4:
+    pass
+elif input_choice == 5:
+    pass
+else :
+    print("Invalid choice. Please try again.")
+
+
+
+
+
+
 
 
 #java -Xmx1024M -Xms1024M -jar server.jar nogui
