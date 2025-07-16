ee2352fd507381f5df8f657123756b7df53b3b2e
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 14:28:56 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,11 @@ if [ "$OS" = "Debian GNU/Linux" ]; then
     curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-2_all.deb
     sudo apt-get install ./zulu-repo_1.0.0-2_all.deb
     sudo apt-get update
+    sudo apt-get install -y zulu8-ca-jre-headless
+    sudo apt-get install -y zulu17-ca-jre-headless
+    sudo apt-get install -y zulu21-ca-jre-headless
 
 
-    
 elif [ "$OS" = "Ubuntu" ]; then
     sudo apt update && sudo apt install -y
     sudo apt install openjdk-8-jre-headless -y
