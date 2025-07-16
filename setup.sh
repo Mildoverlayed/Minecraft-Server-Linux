940958b49644c596614125cf1fb88cb15cf38e20
Mildoverlag <peter.thimas@gmail.com>
2025-07-15 22:11:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ echo "NEW LINE"
 os_name=$(sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/')
 
 if [ -z "$os_name" ]; then
-    echo "Operating system name could not be determined."
-elif 
+    echo "Operating system name could not be determined. And or is not supported."
+elif [ "$os_name" == "Debian GNU/Linux" ]; then
+    echo "Deb"
 fi
