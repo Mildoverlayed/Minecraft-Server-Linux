8b2e462e1d0cce07ac2d64a1def4394a5b405772
Mildoverlag <peter.thimas@gmail.com>
2025-07-15 22:38:37 -0400

    switch form Distro based to package based and add apt support

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-## curent supported Disros 
-* Debian GNU/Linux
+## suported package managers
+* apt
+* dpkg
+* rpm
+* flatpak
+* pacman
 
 
-If your disro is not supported open an issue and add your distro name in the os release file in /etc/os-release
+
+
+
+Java 8: Required for Minecraft versions 1.12 through 1.176.
+Java 17: Required for Minecraft version 1.18 and up6.
+Java 16: Not recommended for Forge versions 1.13 and up due to reflection issues6. These versions ensure compatibility with the modding community and provide a stable environment for running modded Minecraft. It is essential to keep your Java version up to date to avoid compatibility issues with the latest mods and updates.
--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-os_name=$(sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/')
+echo "Before we start, we need to know what package manager to use. 1-5"
+echo "[1.] APT"
+echo "[2.] DPKG"
+echo "[3.] RPM"
+echo "[4.] Flatpak"
+echo "[5.] Pacman"
+read package_manager
+if [ "$package_manager" -eq 1 ]; then
+    apt update && apt upgrade -y
+    apt install openjdk-8-jre-headless -y
+    apt install openjdk-17-jre-headless -y
+    apt install openjdk-21-jre-headless -y
 
-if [ -z "$os_name" ]; then
-    echo "Operating system name could not be determined. And or is not supported."
-elif [ "$os_name" == "Debian GNU/Linux" ]; then
-    echo "Deb"
+
+
+elif [ "$package_manager" -eq 2 ]; then
+    echo "You selected DPKG."
+elif [ "$package_manager" -eq 3 ]; then
+    echo "You selected RPM."
+elif [ "$package_manager" -eq 4 ]; then
+    echo "You selected Flatpak."
+elif [ "$package_manager" -eq 5 ]; then
+    echo "You selected Pacman."
+else
+    echo "Invalid selection. Please run the script again and select a valid option."
+    exit 1
 fi
