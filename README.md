a3803eb5c7dc32c33aa01e80e35224b532a62f7c
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 13:26:45 -0400

    Go back to linux distro based

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-## suported package managers
-* apt
-* dpkg
-* rpm
-* flatpak
-* pacman
-
-
-
-
-
 Java 8: Required for Minecraft versions 1.12 through 1.176.
 Java 17: Required for Minecraft version 1.18 and up6.
-Java 16: Not recommended for Forge versions 1.13 and up due to reflection issues6. These versions ensure compatibility with the modding community and provide a stable environment for running modded Minecraft. It is essential to keep your Java version up to date to avoid compatibility issues with the latest mods and updates.
+Java 21: or latest
--- setup.sh
+++ setup.sh
@@ -1,29 +1,3 @@
-#!/bin/bash
-
-echo "Before we start, we need to know what package manager to use. 1-5"
-echo "[1.] APT"
-echo "[2.] DPKG"
-echo "[3.] RPM"
-echo "[4.] Flatpak"
-echo "[5.] Pacman"
-read package_manager
-if [ "$package_manager" -eq 1 ]; then
-    apt update && apt upgrade -y &&  apt-get update
-    apt install zulu-8-jre-headless -y
-    apt install zulu-17-jre-headless -y
-    apt install openjdk-21-jre-headless -y
-
-
-
-elif [ "$package_manager" -eq 2 ]; then
-    echo "You selected DPKG."
-elif [ "$package_manager" -eq 3 ]; then
-    echo "You selected RPM."
-elif [ "$package_manager" -eq 4 ]; then
-    echo "You selected Flatpak."
-elif [ "$package_manager" -eq 5 ]; then
-    echo "You selected Pacman."
-else
-    echo "Invalid selection. Please run the script again and select a valid option."
-    exit 1
-fi
+cat /etc/os-release
+echo "NEW LINE"
+sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/'
