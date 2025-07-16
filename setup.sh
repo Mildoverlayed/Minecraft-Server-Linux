77543763d95fd9d0d563158f362e9aeb79cbb6a5
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 14:26:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,16 @@ echo "Detected OS: $OS"
 if [ "$OS" = "Debian GNU/Linux" ]; then
     sudo apt update && sudo apt install -y
     sudo apt-get -q update
+    sudo apt-get -yq install gnupg curl
+    sudo apt-key adv \
+    --keyserver hkp://keyserver.ubuntu.com:80 \
+    --recv-keys 0xB1998361219BD9C9
+    curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-2_all.deb
+    sudo apt-get install ./zulu-repo_1.0.0-2_all.deb
+    sudo apt-get update
+
+
+    
 elif [ "$OS" = "Ubuntu" ]; then
     sudo apt update && sudo apt install -y
     sudo apt install openjdk-8-jre-headless -y
