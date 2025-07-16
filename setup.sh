90ff3e5448b6c32294587d6ced8e5c5b8c815cc8
Mildoverlag <peter.thimas@gmail.com>
2025-07-16 14:03:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
-
-
 OS=$(sed -n 's/^NAME=//p' /etc/os-release | sed 's/^"\(.*\)"$/\1/')
 echo "Detected OS: $OS"
 
-if [ $OS == "Debian GNU/Linux" ]; then
+if [ "$OS" = "Debian GNU/Linux" ]; then
     sudo apt update && sudo apt install -y
 
-elif [[ $OS == "Ubuntu" ]]; then
+elif [ "$OS" = "Ubuntu" ]; then
     sudo apt update && sudo apt install -y
-    sudo apt install openjdk-8-jre-headless
-    sudo apt install openjdk-17-jre-headless
-    sudo apt install openjdk-21-jre-headless
+    sudo apt install openjdk-8-jre-headless -y
+    sudo apt install openjdk-17-jre-headless -y
+    sudo apt install openjdk-21-jre-headless -y
 
-elif [[ $OS == "Fedora" ]]; then
+elif [ "$OS" = "Fedora" ]; then
     echo "fed"
-elif [[ $OS == "Arch Linux" ]]; then
+elif [ "$OS" = "Arch Linux" ]; then
     echo "arch"
-elif [[ $OS == "Manjaro Linux" ]]; then
+elif [ "$OS" = "Manjaro Linux" ]; then
     echo "manjaro"
 else
     echo "Unsupported OS: $OS"
