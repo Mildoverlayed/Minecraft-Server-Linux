c6692e3524d66c80a74541988ef64f55c64aab32
Mildoverlag <peter.thimas@gmail.com>
2025-07-17 13:28:23 -0400

    Update tmuxStart.sh

--- tmuxStart.sh
+++ tmuxStart.sh
@@ -5,14 +5,14 @@ SESSION="minecraft-server"
 # Start a new tmux session, split vertically (left/right)
 tmux new-session -d -s $SESSION
 
-# Run gotop in the left pane
-tmux send-keys -t $SESSION 'gotop' C-m
+# Run the Python script in the left pane (pane 0)
+tmux send-keys -t $SESSION 'python3 server.py' C-m
 
 # Split the window vertically (right pane)
 tmux split-window -h -t $SESSION
 
-# Run the Python script in the right pane
-tmux send-keys -t $SESSION:0.1 'python3 server.py' C-m
+# Run gotop in the right pane (pane 1)
+tmux send-keys -t $SESSION:0.1 'gotop' C-m
 
 # Attach to the session
 tmux attach -t $SESSION
