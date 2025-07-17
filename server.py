eccca15964e67a2bd694262d51a05f488efe8467
Mildoverlag <peter.thimas@gmail.com>
2025-07-17 12:25:05 -0400

    Update server.py

--- server.py
+++ server.py
@@ -40,95 +40,100 @@ def conferminput(prompt):
 input_choice = 0
 
 # Start of the script
-ClearScreen()
-
-with open('config.json', 'r') as openfile:
-
-    json_object = json.load(openfile)
-    SETUP = json_object['SETUP']
-    MINRAM = json_object['MINRAM']
-    MAXRAM = json_object['MAXRAM']
-
-if not SETUP:
-    print("Please run the setup first.")
-    exit()
-
-print("Minecraft Server Manager")
-print("choose an option:")
-print("1. Create Instance")
-print("2. Start Instance")
-print("3. Delete Instance")
-print("4. Configure Instance")
-print("5. Global Settings")
-
-
 while True:
-    try:
-        input_choice = int(input())
-        break
-    except ValueError:
+    ClearScreen()
+    
+    with open('config.json', 'r') as openfile:
+    
+        json_object = json.load(openfile)
+        SETUP = json_object['SETUP']
+        MINRAM = json_object['MINRAM']
+        MAXRAM = json_object['MAXRAM']
+    
+    if not SETUP:
+        print("Please run the setup first.")
+        exit()
+    
+    print("Minecraft Server Manager")
+    print("choose an option:")
+    print("1. Create Instance")
+    print("2. Start Instance")
+    print("3. Delete Instance")
+    print("4. Configure Instance")
+    print("5. Global Settings")
+    
+    
+    while True:
+        try:
+            input_choice = int(input())
+            break
+        except ValueError:
+            ClearScreen()
+            print("Minecraft Server Manager")
+            print("choose an option:")
+            print("1. Create Instance")
+            print("2. Start Instance")
+            print("3. Delete Instance")
+            print("4. Configure Instance")
+            print("5. Global Settings")
+            print("\n \n Invalid input. Please enter a number.")
+            continue
+        
+    if input_choice == 1:
+        # Create Instance
         ClearScreen()
-        print("Minecraft Server Manager")
-        print("choose an option:")
-        print("1. Create Instance")
-        print("2. Start Instance")
-        print("3. Delete Instance")
-        print("4. Configure Instance")
-        print("5. Global Settings")
-        print("\n \n Invalid input. Please enter a number.")
-        continue
-
-if input_choice == 1:
-    # Create Instance
-    ClearScreen()
-    pass
-
-elif input_choice == 2:
-    # Start Instance
-    ClearScreen()
-    ListInstances()
-    instance_name = input("Enter the instance name to start: ")
-    if instance_name:
-        instance_path = os.path.join(os.path.dirname(__file__), 'Instances', instance_name)
-        if os.path.exists(instance_path):
-            conferminput("Use default RAM settings? MinRam:{MINRAM} MaxRam{MAXRAM} (Y/n): ")
-
-
-
-            print(f"Starting instance: {instance_name}")
-
-
-        else:
-            print(f"Instance '{instance_name}' does not exist.")
-    else:
-        print("No instance name provided.")
-    pass
-
-elif input_choice == 3:
-    # Delete Instance
-    ClearScreen()
-    pass
-
-elif input_choice == 4:
-    # Configure Instance
-    ClearScreen()
-    ListInstances()
-    instance_name = input("Enter the instance name to configure: ")
-    pass
-
-elif input_choice == 5:
-    # Global Settings
-    ClearScreen()
-    pass
-
-else :
-    print("Invalid choice. Please try again.")
-
-
-
-
-
-
-
-
-#java -Xmx1024M -Xms1024M -jar server.jar nogui
+        pass
+    
+    elif input_choice == 2:
+        # Start Instance
+        ClearScreen()
+        ListInstances()
+        while True:
+            ClearScreen()
+            instance_name = input("Enter the instance name to start: ")
+            if instance_name:
+                instance_path = os.path.join(os.path.dirname(__file__), 'Instances', instance_name)
+                if os.path.exists(instance_path):
+                    print(f"Starting instance: {instance_name}")
+                    os.system(f'java -Xmx{MAXRAM}M -Xms{MINRAM}M -jar "{instance_path}/server.jar" nogui')
+                    break
+                else:
+                    print("Instance not found. Please try again.")
+            else:
+                print("Instance name cannot be empty. Please try again.")
+    
+    elif input_choice == 3:
+        # Delete Instance
+        ClearScreen()
+        if conferminput("Are you sure you want to delete an instance? (Y/n): "):
+            ListInstances()
+            instance_name = input("Enter the instance name to delete: ")
+            instance_path = os.path.join(os.path.dirname(__file__), 'Instances', instance_name)
+            if os.path.exists(instance_path):
+                os.rmdir(instance_path)
+            else:
+                print("Instance not found. Please try again.")
+    
+    elif input_choice == 4:
+        # Configure Instance
+        ClearScreen()
+        ListInstances()
+        instance_name = input("Enter the instance name to configure: ")
+        pass
+    
+    elif input_choice == 5:
+        # Global Settings
+        ClearScreen()
+        pass
+    
+    else :
+        print("Invalid choice. Please try again.")
+    
+    
+    
+    
+    
+    
+    
+    
+    #java -Xmx1024M -Xms1024M -jar server.jar nogui
