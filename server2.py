d28085949ee3dec0226f4f696c7a6ea7b556c7db
Mildoverlag <peter.thimas@gmail.com>
2025-07-18 18:07:28 -0400

    Create server2.py

---
+++ server2.py
@@ -0,0 +1,18 @@
+#
+#
+#
+
+
+# import necessary libraries
+import json
+
+# Define Functions# 
+
+
+
+# Main
+
+with open('config.json', 'r') as json_file:
+    config = json.load(json_file)
+
+    
