f4491b115a5517b7c10a392c18aac74abf30286e
Mildoverlag <peter.thimas@gmail.com>
2025-07-22 22:28:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,8 @@ if [ "$OS" = "Debian GNU/Linux" ]; then
     sudo apt-get update
     sudo apt install tmux -y
     curl -O https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_amd64.deb
-    sudo dpkg -i *.deb
+    sudo apt-get install ./gotop_v4.2.0_linux_amd64.deb
+    sudo apt-get update
     sudo apt-get install jq -y
     sudo apt-get install -y zulu8-ca-jre-headless
     sudo apt-get install -y zulu17-ca-jre-headless
@@ -26,8 +27,9 @@ elif [ "$OS" = "Ubuntu" ]; then
     sudo apt update && sudo apt install -y
     sudo apt-get install jq -y
     sudo apt install tmux -y
-    curl -O https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_amd64.deb 
-    sudo dpkg -i *.deb
+    curl -O https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_amd64.deb
+    sudo apt-get install ./gotop_v4.2.0_linux_amd64.deb
+    sudo apt-get update
     sudo apt install openjdk-8-jre-headless -y
     sudo apt install openjdk-17-jre-headless -y
     sudo apt install openjdk-21-jre-headless -y
